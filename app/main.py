1f97d63be31cd30ff9307ee0e35d2eb35af9e0a8
imisi99 <isongrichard234@gmail.com>
2024-06-30 23:51:42 +0100

    user

--- app/main.py
+++ app/main.py
@@ -3,9 +3,13 @@ from .routers.user import user
 from .model import model as model
 from .model.database import engine
 from .routers.estate import estate
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+)
 app.include_router(user, prefix='/user', tags=['User'])
 app.include_router(estate, prefix='/estate', tags=['Order'])
 
--- app/model/database.py
+++ app/model/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv('DATABASE_URL')
 engine = create_engine(DATABASE_URL)
 begin = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 data = declarative_base()
